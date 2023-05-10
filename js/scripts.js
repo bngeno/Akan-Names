e744bd027f564f24b6bb80329aeec914029b00d4
Benard Ng'eno <BNgeno@safaricom.co.ke>
2023-05-10 12:23:13 +0300

    update all

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua Born on Sunday", "Adwoa Born on Monday", "Abenaa Born on Tuesday", "Akua Born on Wedsday", "Yaa Born on Thursday", "Afua Born on Friday", "Ama Born on Surtuday"];
+    var maleNames = ["Kwasi Born on Sunday", "Kudwo Born on Monday", "Kwabena Born on Tuesday", "Kwaku Born on Wedsday", "Yaw Born on Thursday", "Kofi Born on Friday", "Kwame Born on Surtuday"];
     var d = new Date(y, --m, d);
     if (g === "Female") {
         return d && femaleNames[d.getDay()];
